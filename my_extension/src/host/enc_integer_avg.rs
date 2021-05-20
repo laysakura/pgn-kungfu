7b688e184d44c54560ac30acb4913775365abe09
Sho Nakatani <lay.sakura@gmail.com>
2021-05-20 14:10:30 +0900

    wip

---
+++ my_extension/src/host/enc_integer_avg.rs
@@ -0,0 +1,15 @@
+mod typ;
+
+use pgx::*;
+
+extension_sql!(
+    r#"
+    CREATE AGGREGATE ENCAVG (EncInteger)
+    (
+        sfunc = enc_integer_avg_state_func,
+        stype = IntegerAvgState,
+        finalfunc = enc_integer_avg_final_func,
+        initcond = '{"sum": 0, "n": 0}'
+    );
+    "#
+);
