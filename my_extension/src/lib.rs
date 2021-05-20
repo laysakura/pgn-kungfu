14e99f88da94f456c407e295c58a447a850584d8
Sho Nakatani <lay.sakura@gmail.com>
2021-05-20 13:58:32 +0900

    ディレクトリ構造を更に細分化

--- my_extension/src/lib.rs
+++ my_extension/src/lib.rs
@@ -5,21 +5,6 @@ use pgx::*;
 
 pg_module_magic!();
 
-#[cfg(any(test, feature = "pg_test"))]
-mod tests {
-    use pgx::*;
-
-    use crate::host::integer_avg::IntegerAvgState;
-
-    #[pg_test]
-    fn test_integer_avg_state() {
-        assert_eq!(
-            2,
-            IntegerAvgState::default().acc(1).acc(2).acc(3).finalize()
-        );
-    }
-}
-
 #[cfg(test)]
 pub mod pg_test {
     pub fn setup(_options: Vec<&str>) {
